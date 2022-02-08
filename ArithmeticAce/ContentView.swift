b4d4914754f62ee55a45acd2d82edc3542fff5e8
William Harrington <wharrington@lcs.on.ca>
2022-02-08 16:51:30 -0500

    Retrieved Class Code from Today

--- ArithmeticAce/ContentView.swift
+++ ArithmeticAce/ContentView.swift
@@ -8,9 +8,127 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var multiplicand = Int.random(in: 1...12)
+    @State var multiplier = Int.random(in: 1...12)
+    
+    // This string contains whatever the user types in
+    @State var inputGiven = ""
+    
+    // Has an answer been checked?
+    @State var answerChecked = false
+    
+    // Was the answer given actually correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+    // What is the correct product?
+    var correctProduct: Int {
+        return multiplicand * multiplier
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(spacing: 0) {
+            
+            Spacer ()
+            
+            HStack {
+                Text("✕")
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("\(multiplicand)")
+                    Text("\(multiplier)")
+                }
+            }
+            
+            Divider()
+            
+            HStack {
+                ZStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                    //        CONDITION      true  false
+                        .opacity(answerCorrect == true ? 1.0 : 0.0)
+                    
+                    Image(systemName: "x.square")
+                        .foregroundColor(.red)
+                    //        CONDITION1         AND     CONDITION2         true  false
+                    //       answerChecked = true     answerCorrect = false
+                        .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
+                    
+                    
+                }
+                Spacer()
+                TextField("",
+                          text: $inputGiven)
+                    .multilineTextAlignment(.trailing)
+            }
+            
+            ZStack {
+                
+                Button(action: {
+                    
+                    // Answer has been checked!
+                    answerChecked = true
+                    
+                    // Convert the input given to an integer, if possible
+                    guard let productGiven = Int(inputGiven) else {
+                        // Sadness, not a number
+                        answerCorrect = false
+                        return
+                    }
+                    
+                    // Check the answer!
+                    if productGiven == correctProduct {
+                        // Celebrate! 👍🏼
+                        answerCorrect = true
+                    } else {
+                        // Sadness, they gave a number, but it's correct 😭
+                        answerCorrect = false
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                // Only show this button when an answer has not been checked
+                    .opacity(answerChecked == false ? 1.0 : 0.0)
+                
+                Button(action: {
+                    // Generate a new question
+                    multiplicand = Int.random(in: 1...12)
+                    multiplier = Int.random(in: 1...12)
+                    
+                    // Reset properties that track what's happening with the current question
+                    answerChecked = false
+                    answerCorrect = false
+                    
+                    // Reset the input field
+                    inputGiven = ""
+                }, label: {
+                    Text("New question")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                // Only show this button when an answer has been checked
+                    .opacity(answerChecked == true ? 1.0 : 0.0)
+                
+            }
+            
+            
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+        .font(.system(size: 45))
+        
+        
     }
 }
 
